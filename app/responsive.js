5b2574fd467b3f9e78cf4bad72c491a4244c900d
Javier18121998 <Javier.Marin.Garcia.Linkedin01@hotmail.com>
2022-08-10 23:57:38 -0500

    ready

--- app/responsive.js
+++ app/responsive.js
@@ -1,15 +1,46 @@
 window.onload = function(){
+    push_a_service();
+    nav_responsive();
+}
+const nav_responsive = function(){
     const menu_btn = document.querySelector('.hamburger');
     const pages_container = document.querySelector('.mobile-nav');
-    const get_off = document.querySelector('#mobile-out');
-    const get_a_off = document.querySelector('.nav_a');
     menu_btn.addEventListener('click', function(){
         menu_btn.classList.toggle('is-active');
         pages_container.classList.toggle('is-active');
     });
-    const get_out = document.querySelector('.nav_out');
-    get_out.addEventListener('click', function(){
-        get_off.classList.toggle('.mobile-out');
-        get_a_off.classList.toggle('.nav_a');
+}
+const push_a_service = function(){
+    const pushing = document.querySelector('.nav_out');
+    const pushing1 = document.querySelector('.nav_out1');
+    const pushing2 = document.querySelector('.nav_out2');
+    const pushing3 = document.querySelector('.nav_out3');
+    const pushing4 = document.querySelector('.nav_out4');
+    const pushing5 = document.querySelector('.nav_out5');
+    const pages_container = document.querySelector('.mobile-nav');
+    const menu_btn = document.querySelector('.hamburger');
+    pushing.addEventListener('click', function(){
+        pages_container.classList.toggle('is-active');
+        menu_btn.classList.toggle('is-active');
+    });
+    pushing1.addEventListener('click', function(){
+        pages_container.classList.toggle('is-active');
+        menu_btn.classList.toggle('is-active');
+    });
+    pushing2.addEventListener('click', function(){
+        pages_container.classList.toggle('is-active');
+        menu_btn.classList.toggle('is-active');
+    });
+    pushing3.addEventListener('click', function(){
+        pages_container.classList.toggle('is-active');
+        menu_btn.classList.toggle('is-active');
+    });
+    pushing4.addEventListener('click', function(){
+        pages_container.classList.toggle('is-active');
+        menu_btn.classList.toggle('is-active');
+    });
+    pushing5.addEventListener('click', function(){
+        pages_container.classList.toggle('is-active');
+        menu_btn.classList.toggle('is-active');
     });
 }
